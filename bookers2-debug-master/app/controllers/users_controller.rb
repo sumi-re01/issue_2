5d1a81be344bf0628493563864213c828ee6cfaa
sumi-re01 <owob.viola@gmail.com>
2021-03-01 11:12:00 +0000

    [Add]他のユーザー情報の編集画面に飛べない

--- bookers2-debug-master/app/controllers/users_controller.rb
+++ bookers2-debug-master/app/controllers/users_controller.rb
@@ -13,6 +13,11 @@ class UsersController < ApplicationController
   end
   def edit
     @user = User.find(params[:id])
+    if @user != current_user
+      redirect_to user_path(current_user)
+    else
+      render "edit"
+    end
   end
 
   def update
